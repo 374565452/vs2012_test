cdb2c3e38785042ee7c97b5cc8af5142190dac64
374565452 <kong42315@163.com>
2016-12-30 11:17:16 +0800

    添加一个按钮

--- test.git.demo/Form1.cs
+++ test.git.demo/Form1.cs
@@ -16,5 +16,10 @@ namespace test.git.demo
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
